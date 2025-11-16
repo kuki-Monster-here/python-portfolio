4cec1e959a97efa2ed206e755f199aadec1d2dca
lumonangel <annamaliaDead@icloud.com>
2025-11-16 16:13:19 +0300

    min_max_finder.py

---
+++ min_max_finder.py
@@ -0,0 +1,9 @@
+def find_min(numbers):
+    return min(numbers)
+
+def find_max(numbers):
+    return max(numbers)
+
+numbers = [5, 2, 8, 1, 9]
+print("Минимальное:", find_min(numbers))
+print("Максимальное:", find_max(numbers))
